7dd4112222eda3b482142f5e2ab3c9671a643a3e
Túlio Amaral <tuliosantos@gee.inatel.br>
2022-01-23 20:58:15 -0300

    add validation to all fields to be filled when creating an user

--- src/modules/users/controller/CreateUserController.ts
+++ src/modules/users/controller/CreateUserController.ts
@@ -1,10 +1,15 @@
 import { Response, Request } from 'express'
 
 import CreateUserService from '../services/CreateUserService';
+import AppError from '../../../utils/AppError'
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, age, role } = request.body
+
+    if(!name || !age || !role) {
+      throw new AppError('All fields must be filled')
+    }
 
     await CreateUserService.execute({ name, age, role })
 
